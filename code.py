0cd1eea2a70143abc6b2ae4c5ad7d80e8c1d829c
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2018-10-23 10:59:41 -0400

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,11 @@ x = 2
 y = 0.4
 newx = 200
 newy = 200
+time_counter = 180
+sky_color = [20,60,105]
+cloud_color = [125,125,125]
+sun_color = [255,255,0]
+light_brightness = 0.6
 def setup():
     size(600,600)
     
@@ -10,6 +15,30 @@ def draw():
     global y
     global newx
     global newy
+    global time_counter
+    
+    global sky_color
+    global cloud_color
+    global sun_color
+    global light_brightness
+    
+    #time
+    time_counter += 0.2
+    if sky_color[2] + light_brightness <= 255 and sky_color[2] + light_brightness >= 0:
+        sky_color[0]+=light_brightness
+        sky_color[1]+=light_brightness
+        sky_color[2]+=light_brightness
+        cloud_color[0]+=light_brightness
+        cloud_color[1]+=light_brightness
+        cloud_color[2]+=light_brightness
+    if time_counter >= 235:
+        time_counter = 175
+    if time_counter == 175:
+        light_brightness *= -1
+        if sun_color[2] == 0:
+            sun_color[2] = 210
+        else:
+            sun_color[2] = 0
     
     #movement
     if newx == 520 or newx == 80:
@@ -21,14 +50,11 @@ def draw():
     
     noStroke()
     clear()
-    background(170,210,255)
     
-    #sun
-    fill(255,255,0)
-    ellipse(80,80,60,60)
+    background(*sky_color)
     
     #cloud
-    fill(255,255,255)
+    fill(*cloud_color)
     ellipse(newx+15, newy, 100, 40)
     ellipse(newx-15, newy, 100, 40)
     ellipse(newx+15, newy-20, 60, 25)
@@ -44,3 +70,9 @@ def draw():
     rect(270,460,60,60)
     fill(255,50,50)
     triangle(260,460,300,420,340,460)
+    
+    #sun
+    translate(width/2, 800)
+    rotate(radians(time_counter))
+    fill(*sun_color)
+    ellipse(300, 600,60,60)
