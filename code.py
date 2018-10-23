f116d7844f1482fcf543ceca991b99adc8b0aaf8
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2018-10-23 09:34:47 -0400

    Update code.py

--- code.py
+++ code.py
@@ -4,33 +4,42 @@ newx = 200
 newy = 200
 def setup():
     size(600,600)
+    
 def draw():
     global x
     global y
     global newx
     global newy
+    
+    #movement
     if newx == 520 or newx == 80:
         x *= -1
     if newy > 204 or newy < 196:
         y *= -1
     newx += x
     newy += y
+    
     noStroke()
     clear()
     background(170,210,255)
+    
+    #sun
     fill(255,255,0)
     ellipse(80,80,60,60)
     
+    #cloud
     fill(255,255,255)
     ellipse(newx+15, newy, 100, 40)
     ellipse(newx-15, newy, 100, 40)
     ellipse(newx+15, newy-20, 60, 25)
     ellipse(newx-15, newy-20, 60, 25)
     
+    #ground
     fill(50,200,50)
     ellipse(300,500,700,50)
     rect(0,500,600,100)
     
+    #house
     fill(255,255,50)
     rect(270,460,60,60)
     fill(255,50,50)
