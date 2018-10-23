a933a15705510eaffdd3e5f49e31eaff4b746f23
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2018-10-23 09:32:46 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,37 @@
+x = 2
+y = 0.4
+newx = 200
+newy = 200
+def setup():
+    size(600,600)
+def draw():
+    global x
+    global y
+    global newx
+    global newy
+    if newx == 520 or newx == 80:
+        x *= -1
+    if newy > 204 or newy < 196:
+        y *= -1
+    newx += x
+    newy += y
+    noStroke()
+    clear()
+    background(170,210,255)
+    fill(255,255,0)
+    ellipse(80,80,60,60)
+    
+    fill(255,255,255)
+    ellipse(newx+15, newy, 100, 40)
+    ellipse(newx-15, newy, 100, 40)
+    ellipse(newx+15, newy-20, 60, 25)
+    ellipse(newx-15, newy-20, 60, 25)
+    
+    fill(50,200,50)
+    ellipse(300,500,700,50)
+    rect(0,500,600,100)
+    
+    fill(255,255,50)
+    rect(270,460,60,60)
+    fill(255,50,50)
+    triangle(260,460,300,420,340,460)
