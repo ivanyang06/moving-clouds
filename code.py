62d7f9e9216ad3395d00095e79d67f7c9a2a51b3
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2018-10-24 14:04:55 -0400

    Update code.py

--- code.py
+++ code.py
@@ -3,12 +3,15 @@ y = 0.4
 randx = 0
 randy = 0
 newx = 200
-newy = 200
+newy = 300
 time_counter = 180
 sky_color = [60,100,145]
-cloud_color = [145,145,145]
+cloud_color = [185,185,185]
 sun_color = [255,255,0]
 light_brightness = 0.6
+moonwidth = 0
+a = -1
+mooncounter = 0
 def setup():
     size(600,600)
     
@@ -20,6 +23,9 @@ def draw():
     global newx
     global newy
     global time_counter
+    global moonwidth
+    global a
+    global mooncounter
     
     global sky_color
     global cloud_color
@@ -50,7 +56,7 @@ def draw():
     #movement
     if newx == 520 or newx == 80:
         x *= -1
-    if newy > 204 or newy < 196:
+    if newy > 304 or newy < 296:
         y *= -1
     newx += x
     newy += y
@@ -78,8 +84,61 @@ def draw():
     fill(255,50,50)
     triangle(260,460,300,420,340,460)
     
-    #sun
+    #sun/moon
     translate(width/2, 800)
     rotate(radians(time_counter))
     fill(*sun_color)
     ellipse(300, 600,60,60)
+    
+    if time_counter == 175 and sun_color[2] == 210:
+        mooncounter += 1
+    if mooncounter == 12:
+        mooncounter = 0
+    print(mooncounter)
+    fill(*sun_color)
+    ellipse(300, 600, 60, 60)
+    fill(*sky_color)
+    if mooncounter == 0: #new moon
+        ellipse(300,600,60,60)
+    if mooncounter == 1:
+        rect(300,540,60,120)
+        ellipse(300,600,40,60)
+    if mooncounter == 2:
+        rect(300,540,60,120)
+        ellipse(300,600,20,60)
+    if mooncounter == 3: #half moon
+        rect(300,540,60,120)
+        ellipse(0,0,0,0)
+    if mooncounter == 4:
+        rect(300,540,60,120)
+        fill(*sun_color)
+        ellipse(300,600,20,60)
+    if mooncounter == 5:
+        rect(300,540,60,120)
+        fill(*sun_color)
+        ellipse(300,600,40,60)
+    if mooncounter == 6: # full moon
+        fill(*sun_color)
+        ellipse(300,600,60,60)
+    if mooncounter == 7:
+        rect(240,540,60,120)
+        fill(*sun_color)
+        ellipse(300,600,40,60)
+    if mooncounter == 8:
+        rect(240,540,60,120)
+        fill(*sun_color)
+        ellipse(300,600,20,60)
+    if mooncounter == 9: #half moon
+        rect(240,540,60,120)
+        ellipse(0,0,0,0)
+    if mooncounter == 10:
+        rect(240,540,60,120)
+        ellipse(300,600,20,60)
+        rect(240,540,60,120)
+    if mooncounter == 11:
+        rect(240,540,60,120)
+        ellipse(300,600,40,60)
+    
+    if sun_color[2] == 0:
+        fill(*sun_color)
+        ellipse(300, 600,60,60)
